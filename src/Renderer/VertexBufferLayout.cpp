bbc0316f7be02c41bd37a8b03e4e9f867d2ebd8a
Volodya132 <smail.vovazlv@gmail.com>
2021-01-11 19:55:33 +0200

    Added VertexArray

---
+++ src/Renderer/VertexBufferLayout.cpp
@@ -0,0 +1,21 @@
+#include "VertexBufferLayout.h"
+
+namespace Renderer {
+	
+	VertexBufferLayout::VertexBufferLayout()
+		: m_stride(0)
+	{
+
+	}
+
+	void VertexBufferLayout::reserveElements(const size_t count)
+	{
+		m_layoutElements.reserve(count);
+	}
+
+	void VertexBufferLayout::addElementLayoutFloat(const unsigned int count, const bool normalized)
+	{
+		m_layoutElements.push_back({ static_cast<GLint>(count), GL_FLOAT, normalized,  count * sizeof(GLfloat) });
+		m_stride += m_layoutElements.back().size;
+	}
+};
---
+++ src/Renderer/VertexBufferLayout.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <vector>
+
+#include <glad/glad.h>
+
+
+namespace Renderer {
+
+	struct  VertexBufferLayoutElement
+	{
+		GLint count;
+		GLenum type;
+		GLboolean normalized;
+		unsigned int size;
+	};
+	class VertexBufferLayout 
+	{
+	public:
+		VertexBufferLayout();
+
+		void reserveElements(const size_t count);
+		unsigned int getStride()const { return m_stride; }
+		void addElementLayoutFloat(const unsigned int count, const bool normalized);
+		const std::vector<VertexBufferLayoutElement>& getLayoutElements()const { return m_layoutElements; }
+
+	public:
+		std::vector<VertexBufferLayoutElement> m_layoutElements;
+		unsigned int m_stride;
+	};
+}
